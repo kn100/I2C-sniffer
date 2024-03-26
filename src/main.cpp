55250ff08573683bb25ab66215f06f33f64a93e3
kn100 <kn100@kn100.me>
2024-03-25 22:34:30 -0400

    more cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #include <Arduino.h>
 
-#define PIN_SDA 12 
+#define PIN_SDA 12
 #define PIN_SCL 13
 
 #define I2C_IDLE 0
@@ -18,80 +18,72 @@ static volatile byte dataBuffer[100000];
 static volatile uint16_t bufferPoiW = 0;
 // points to the position where we've read up until
 static uint16_t bufferPoiR = 0;
-// counter of bits that have appeared on the bus
+// Bits that have appeared on the bus
 static volatile byte bitCount = 0;
 // counter of bytes were writen in one communication
 static volatile uint16_t byteCount = 0;
-// Container of the actual SDA bit
-static volatile byte i2cBitD = 0;
-// Container of the actual SDA bit	   
-static volatile byte i2cBitD2 = 0;
-// Container of the actual SDA bit		   
-static volatile byte i2cBitC = 0;
-// Container of the actual SCL bit
-static volatile byte i2cClk = 0;
-// Container of the last ACK value
-static volatile byte i2cCase = 0;
 // Counter of false start events
 static volatile uint16_t falseStart = 0;
 
 // Statistical variables
-static volatile uint16_t sclUpCnt = 0;	   
+static volatile uint16_t sclUpCnt = 0;
 static volatile uint16_t sdaUpCnt = 0;
 static volatile uint16_t sdaDownCnt = 0;
 
 void IRAM_ATTR i2cTriggerOnRaisingSCL()
 {
 	sclUpCnt++;
-
+	// Unclear to me what a false start is. Maybe return here.
 	if (i2cStatus == I2C_IDLE)
 	{
-		// Unclear to me what a false start is. Maybe return here.
 		falseStart++;
 	}
 
-	i2cBitC = digitalRead(PIN_SDA);
+	byte i2cBitC = digitalRead(PIN_SDA);
 
 	// decide where we are and what to do with incoming data
-	i2cCase = 0; // normal case
+	byte i2cCase = 0; // normal case
 
 	if (bitCount == 8) // ACK case
+	{
 		i2cCase = 1;
+	}
 
 	if (bitCount == 7 && byteCount == 0) // R/W if the first address byte
+	{
 		i2cCase = 2;
+	}
 
 	bitCount++;
 
 	switch (i2cCase)
 	{
-	case 0:										  // normal case
-		dataBuffer[bufferPoiW++] = '0' + i2cBitC; // 48
-		break;									  // end of case 0 general
-	case 1:										  // ACK
-		if (i2cBitC)							  // 1 NACK SDA HIGH
-		{
-			dataBuffer[bufferPoiW++] = '-'; // 45
-		}
-		else // 0 ACK SDA LOW
-		{
-			dataBuffer[bufferPoiW++] = '+'; // 43
-		}
+	case 0: // Normal case
+		dataBuffer[bufferPoiW++] = '0' + i2cBitC;
+		break;
+	case 1: // ACK case (happens at the end of an address or a data byte)
+		dataBuffer[bufferPoiW++] = (i2cBitC ? '-' : '+');
 		byteCount++;
 		bitCount = 0;
-		break; // end of case 1 ACK
-	case 2:
-		if (i2cBitC)
-		{
-			dataBuffer[bufferPoiW++] = 'R'; // 82
-		}
-		else
-		{
-			dataBuffer[bufferPoiW++] = 'W'; // 87
-		}
-		break; // end of case 2 R/W
+		break;
+	case 2: // Whether the primary wants to Read or Write to the secondary
+		dataBuffer[bufferPoiW++] = (i2cBitC ? 'R' : 'W');
+		break;
+	}
+}
 
-	} // end of switch
+// Continuously polls busPin until it has numReadings stable readings, then returns the reading
+bool IRAM_ATTR busStableRead(int busPin, int numReadings)
+{
+	int reading = digitalRead(busPin);
+	for (int i = 0; i < numReadings; i++)
+	{
+		if (digitalRead(busPin) != reading)
+		{
+			i = 0;
+		}
+	}
+	return reading;
 }
 
 /**
@@ -103,38 +95,30 @@ void IRAM_ATTR i2cTriggerOnRaisingSCL()
  */
 void IRAM_ATTR i2cTriggerOnChangeSDA()
 {
-	// make sure that the SDA is in stable state
-	do
+	if (busStableRead(PIN_SCL, 2))
 	{
-		i2cBitD = digitalRead(PIN_SDA);
-		i2cBitD2 = digitalRead(PIN_SDA);
-	} while (i2cBitD != i2cBitD2);
-
-	if (i2cBitD) // RISING if SDA is HIGH (1)
-	{
-		i2cClk = digitalRead(PIN_SCL);
+		byte i2cClk = digitalRead(PIN_SCL);
 		if (i2cStatus = !I2C_IDLE && i2cClk == 1) // If SCL still HIGH then it is a STOP sign
 		{
-			// i2cStatus = I2C_STOP;
 			i2cStatus = I2C_IDLE;
 			bitCount = 0;
 			byteCount = 0;
 			bufferPoiW--;
-			dataBuffer[bufferPoiW++] = 's';	 // 115
-			dataBuffer[bufferPoiW++] = '\n'; // 10
+			dataBuffer[bufferPoiW++] = 's';
+			dataBuffer[bufferPoiW++] = '\n';
 		}
 		sdaUpCnt++;
 	}
 	else // FALLING if SDA is LOW
 	{
-		i2cClk = digitalRead(PIN_SCL);
+		byte i2cClk = digitalRead(PIN_SCL);
 		if (i2cStatus == I2C_IDLE && i2cClk) // If SCL still HIGH than this is a START
 		{
 			i2cStatus = I2C_TRX;
 			bitCount = 0;
 			byteCount = 0;
-			dataBuffer[bufferPoiW++] = 'S'; // 83 STOP
-											// i2cStatus = START;
+			// Stop condition
+			dataBuffer[bufferPoiW++] = 'S';
 		}
 		sdaDownCnt++;
 	}
@@ -156,7 +140,7 @@ void processDataBuffer()
 
 	uint16_t pw = bufferPoiW;
 
-	Serial.printf("\nSCL up: %d SDA up: %d SDA down: %d false start: %d\n", sclUpCnt, sdaUpCnt, sdaDownCnt, falseStart);
+	// Serial.printf("\nSCL up: %d SDA up: %d SDA down: %d false start: %d\n", sclUpCnt, sdaUpCnt, sdaDownCnt, falseStart);
 
 	for (int i = bufferPoiR; i < pw; i++)
 	{
@@ -189,9 +173,5 @@ void loop()
 	if (i2cStatus == I2C_IDLE)
 	{
 		processDataBuffer();
-		Serial.print("\rStart delay    ");
-		delay(5000);
-		Serial.print("\rEnd delay    ");
-		delay(500);
 	}
 }
